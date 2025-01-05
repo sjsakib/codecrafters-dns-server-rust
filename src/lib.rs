9baf0032a97974cee8b99baf20b7b338a777bbb7
Sakib <sjsakib.bd@gmail.com>
2025-01-06 00:18:15 +0600

    parse headers

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,57 @@ fn split_u16(n: u16) -> (u8, u8) {
 pub struct Message {
     head: [u8; 12],
     questions: Vec<BytesMut>,
-    answers: Vec<BytesMut>
+    answers: Vec<BytesMut>,
 }
-
 
 impl Message {
     pub fn new() -> Message {
         Message {
             head: [0; 12],
             questions: vec![],
-            answers: vec![]
+            answers: vec![],
         }
     }
+
+    pub fn from_buf(buf: &[u8]) -> Message {
+        let mut head = [0; 12];
+
+        head.copy_from_slice(buf);
+
+        Message {
+            head,
+            questions: vec![],
+            answers: vec![],
+        }
+    }
+
+    fn put_bit(&mut self, offset: usize, f: bool) {
+        let byte_idx = offset / 8;
+        let bit_idx = offset % 8;
+
+        if f {
+            self.head[byte_idx] |= 1u8 << (7 - bit_idx);
+        } else {
+            self.head[byte_idx] &= !(1u8 << (7 - bit_idx));
+        }
+    }
+
+    fn put_u4(&mut self, offset: usize, n: u8) {
+        let byte_idx = offset / 8;
+        let bit_offset = (offset % 8);
+
+        self.head[byte_idx] &= !(0b00001111 << (4 - bit_offset));
+
+        self.head[byte_idx] |= n << (4 - bit_offset);
+    }
+
+    fn get_u4(&self, offset: usize) -> u8 {
+        let byte_idx = offset / 8;
+        let bit_offset = (offset % 8);
+
+        (self.head[byte_idx] & (0b00001111 << (4 - bit_offset))) >> (4 - bit_offset)
+    }
+
     pub fn get_bytes(&self) -> Vec<u8> {
         let mut res = vec![];
         res.append(&mut self.head.to_vec());
@@ -39,11 +78,19 @@ impl Message {
     }
 
     pub fn qr(&mut self, f: bool) {
-        if f {
-            self.head[2] |= 0b10000000;
-        } else {
-            self.head[2] &= 0b01111111;
-        }
+        self.put_bit(16, f);
+    }
+
+    pub fn set_rcode(&mut self, rcode: u8) {
+        self.put_u4(28, rcode);
+    }
+
+    pub fn get_opcode(&self) -> u8 {
+        self.get_u4(17)
+    }
+
+    pub fn get_rcode(&self) -> u8 {
+        self.get_u4(28)
     }
 
     fn set_qd_count(&mut self, count: u16) {
@@ -56,6 +103,12 @@ impl Message {
         let (high, low) = split_u16(count);
         self.head[6] = high;
         self.head[7] = low;
+    }
+
+    fn set_opcode(&mut self, high: u8, low: u8) {
+        self.head[2] &= 0x80; // clear last 7 bits
+        self.head[2] |= high >> 1;
+        self.head[3] &= 1;
     }
 
     pub fn add_question(&mut self) {
@@ -83,8 +136,6 @@ impl Message {
         answer.put_u8(76);
         answer.put_u8(21);
         answer.put_u8(21);
-
-
 
         self.answers.push(answer);
         self.set_a_count(self.answers.len() as u16);
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::net::UdpSocket;
 use codecrafters_dns_server::Message;
+use std::net::UdpSocket;
 
 fn main() {
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
@@ -9,11 +9,14 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let mut response = Message::new();
+                let mut response = Message::from_buf(&buf[0..12]);
                 response.id_from_buf(&buf);
                 response.qr(true);
                 response.add_question();
                 response.add_answer();
+                let opcode = if response.get_opcode() == 0u8 { 0 } else { 4u8 };
+                println!("Found rcode: {}", opcode);
+                response.set_rcode(opcode);
                 udp_socket
                     .send_to(&response.get_bytes(), source)
                     .expect("Failed to send response");
